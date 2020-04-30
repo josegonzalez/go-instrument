e2504a282e154aadf713aeff94ddd38bc65762df
Zach Hammer <zhammer@seatgeek.com>
2020-04-30 15:45:56 -0400

    remove newrelic (#5)
    * try removing newrelic transaction commands all together
    
    (will need to check how much work this is to get into PB)
    
    * Revert "try removing newrelic transaction commands all together"
    
    This reverts commit 7f1fcc48024abadb8a236b0fe5b075143f2a719f.
    
    * remove newrelic but keep interfaces the same

--- config.go
+++ config.go
@@ -1,11 +1,10 @@
 package instrument
 
 import (
-	log "github.com/sirupsen/logrus"
-	"github.com/newrelic/go-agent"
-	"github.com/newrelic/go-agent/_integrations/nrlogrus"
-	"github.com/seatgeek/telemetria"
 	"os"
+
+	"github.com/seatgeek/telemetria"
+	log "github.com/sirupsen/logrus"
 )
 
 // DefaultConfig Returns the InstrumentsCofig that is most suitable
@@ -25,40 +24,9 @@ func DefaultConfig(appName string) *InstrumentsConfig {
 		panic(err)
 	}
 
-	nr := setupNewRelic(appName)
-
 	return &InstrumentsConfig{
 		statsRecorder: recorder,
-		app:           nr,
 	}
-}
-
-func setupNewRelic(appName string) newrelic.Application {
-	newrelicKey := os.Getenv("NEW_RELIC_LICENSE")
-	newrelicName := os.Getenv("NEW_RELIC_NAME")
-	shouldDisable := os.Getenv("NEW_RELIC_DISABLE")
-
-	if newrelicName == "" {
-		newrelicName = appName
-	}
-
-	config := newrelic.NewConfig(newrelicName, newrelicKey)
-	config.CrossApplicationTracer.Enabled = false
-	config.DistributedTracer.Enabled = true
-
-	if checkBool(shouldDisable) {
-		config.Enabled = false
-	}
-
-	config.Logger = nrlogrus.StandardLogger()
-	app, err := newrelic.NewApplication(config)
-
-	if err != nil {
-		log.Fatalf("Invalid New Relic app name or license key: %s", err)
-		panic(err)
-	}
-
-	return app
 }
 
 func checkBool(str string) bool {
